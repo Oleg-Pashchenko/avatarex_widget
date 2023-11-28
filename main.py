c66e210d9eacecab970227d306306a10a906197a
olehandro <odpash.itmo@gmail.com>
2023-11-28 03:06:36 +0300

    added cors support

--- main.py
+++ main.py
@@ -4,19 +4,15 @@ from flask_cors import CORS, cross_origin
 import openai_connector
 
 app = Flask(__name__)
-cors = CORS(app)
-CORS(app, supports_credentials=True, resources={r"/*": {"origins": "http://95.140.146.86"}})
-app.config['CORS_HEADERS'] = 'Content-Type'
+cors = CORS(app, supports_credentials=True, resources={r"/*": {"origins": "*", "headers": "Content-Type"}})
 
-
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/', methods=['POST'])
 @cross_origin()
 def main():
     post_params = request.get_json()
     print(post_params)
     response = {'answer': openai_connector.knowledge_mode(post_params['messages'])}
-    return response
-
+    return jsonify(response)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8083, debug=True)
