d81bbeda65b21cf4f408fcd57f89939915ff303d
olehandro <odpash.itmo@gmail.com>
2023-11-28 04:00:33 +0300

    added cors support

--- main.py
+++ main.py
@@ -1,28 +1,21 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-
 import openai_connector
+import json
 
 app = Flask(__name__)
- #CORS(app, supports_credentials=True)
+CORS(app, supports_credentials=True)
 
 
 @app.route('/', methods=['POST'])
 def main():
-    post_params = request.get_json()
+    post_data = request.get_data(as_text=True)
+    post_params = json.loads(post_data)
+
     print(post_params)
     response = {'answer': openai_connector.knowledge_mode(post_params['messages'])}
 
-    # Flask-CORS will handle CORS headers, so you don't need to add them manually
-
     return jsonify(response)
-
-
-# @app.after_request
-def add_headers(response):
-    response.headers.add('Access-Control-Allow-Origin', '*')
-    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
-    return response
 
 
 if __name__ == '__main__':
