c1fd06362442c03ccf5ce512a2933dc3a87d1f85
olehandro <odpash.itmo@gmail.com>
2023-11-30 00:07:44 +0300

    added cors support

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route('/widget-api', methods=['POST'])
 def main():
     print('yes')
-    post_params = request.get_data()
+    post_params = request.values.to_dict()
     print(post_params)
     response = {'answer': openai_connector.knowledge_mode(post_params['messages'])}
     return jsonify(response)
